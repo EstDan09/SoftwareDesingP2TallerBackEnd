2b8d1be9a7518eb1f836168572f0875b938c88ba
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2025-01-22 10:40:19 -0600

    basic done

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,55 +1,58 @@
 const Usuario = require("../models/Usuario");
-const usuario = require("../routes/usuario");
 
 exports.crearUsuario = async (req, res) => {
-    try {
-        if (!req.body.correo || !req.body.password || !req.body.nombre) {
-            return res.status(400).json({ msg: "Todos los campos son obligatorios." });
-        }
-
-        const newUsuario = {
-            nombre: req.body.nombre,
-            correo: req.body.correo,
-            password: req.body.password
-        }
-
-        let usuario = new Usuario(newUsuario);
-
-        await usuario.save();
-
-        res.send({ usuario });
-
-    } catch (error) {
-        if (error.code === 11000) {
-            res.status(409).send("El correo ya existe");
-        } else {
-            res.status(500).send("Hubo un error al crear un usuario");
-        }
+  try {
+    if (!req.body.correo || !req.body.password || !req.body.nombre) {
+      return res.status(400).json({ msg: "Todos los campos son obligatorios." });
     }
-}
+
+    // Verifica si se recibió una foto de perfil
+    const fotoPerfil = req.body.fotoPerfil || null;
+
+    const newUsuario = {
+      nombre: req.body.nombre,
+      correo: req.body.correo,
+      password: req.body.password,
+      fotoPerfil: fotoPerfil, // Guarda la imagen en Base64
+    };
+
+    let usuario = new Usuario(newUsuario);
+    await usuario.save();
+
+    res.send({ usuario });
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(409).send("El correo ya existe");
+    } else {
+      res.status(500).send("Hubo un error al crear un usuario");
+    }
+  }
+};
 
 exports.obtenerUsuarios = async (req, res) => {
-    try {
-        const usuarios = await Usuario.find();//.populate('carros'); 
-        res.status(200).json({ msg: "Lista de usuarios-votos obtenida exitosamente.", data: usuarios });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Hubo un error al obtener los usuarios");
-    }
+  try {
+    const usuarios = await Usuario.find(); //.populate('carros'); 
+    res.status(200).json({
+      msg: "Lista de usuarios obtenida exitosamente.",
+      data: usuarios,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Hubo un error al obtener los usuarios");
+  }
 };
 
 exports.obtenerUsuario = async (req, res) => {
-    try {
-        if (!req.params.id) {
-            return res.status(400).json ({msg: "Se reuqiere ID de usuario."})
-        }
-        
-        const user = await Usuario.findById(req.params.id);
-
-        res.status(200).json({ msg: "Usuario obtenido exitosamente.", data: user });
-
-    } catch (error){
-        console.error(error);
-        res.status(500).send("Hubo un error al obtener el usuario");
+  try {
+    if (!req.params.id) {
+      return res.status(400).json({ msg: "Se requiere ID de usuario." });
     }
-}
+
+    const user = await Usuario.findById(req.params.id);
+
+    res.status(200).json({ msg: "Usuario obtenido exitosamente.", data: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Hubo un error al obtener el usuario");
+  }
+};
